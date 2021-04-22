a97451ef58d5cbf95bad4fcf6a3a3b00687a138b
PUYUP <hellopuyup@gmail.com>
2021-04-22 19:40:54 +0700

    Test

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -47,12 +47,13 @@ X_FRAME_OPTIONS = 'DENY'
 # Django csrf
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/ref/csrf/
-CSRF_COOKIE_DOMAIN = None
+CSRF_COOKIE_DOMAIN = '.storeebarbershop.com'
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 CSRF_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_TRUSTED_ORIGINS = [
+    '.storeebarbershop.com',
     'storee-46b6e.web.app'
 ]
 
