ad52257d614a5cfb982654db5b225875f0b956f5
PUYUP <hellopuyup@gmail.com>
2021-06-02 20:54:58 +0700

    test

--- setup/settings/project.py
+++ setup/settings/project.py
@@ -110,8 +110,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
