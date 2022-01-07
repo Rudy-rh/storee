b598c87c81e4b86204c6344f66d4885ddc4dc1b4
PUYUP <hellopuyup@gmail.com>
2022-01-07 22:32:47 +0700

    test

--- apps/barber/models/order.py
+++ apps/barber/models/order.py
@@ -73,7 +73,7 @@ class AbstractOrder(AbstractCommonField):
         msisdn = '0811806807'
         send_date = timezone.datetime.today() + timedelta(seconds=5)
         send_thanks_to_customer_whatsapp.apply_async(args=[msisdn], eta=send_date)
-        add.apply_acync((1, 3), eta=send_date)
+        add.apply_async((1, 3), eta=send_date)
 
         return super().save(*args, **kwargs)
 
