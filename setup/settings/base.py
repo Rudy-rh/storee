072e87cefa48c699681a491b5802cb7c030f198d
PUYUP <hellopuyup@gmail.com>
2022-01-07 22:36:30 +0700

    test

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -92,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 # LANGUAGE_CODE = 'id-ID'
-TIME_ZONE = 'Asia/Jayapura'
+TIME_ZONE = 'Asia/Jakarta'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
