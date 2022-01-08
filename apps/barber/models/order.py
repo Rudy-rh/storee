4361b4e7c8c0f643d2ea1ebb0b71304c1e150593
PUYUP <hellopuyup@gmail.com>
2022-01-08 10:24:31 +0700

    test

--- apps/barber/models/order.py
+++ apps/barber/models/order.py
@@ -70,15 +70,10 @@ class AbstractOrder(AbstractCommonField):
             if branch:
                 self.branch = branch
 
-        """
         msisdn = '0811806807'
         send_date = timezone.datetime.today() + timedelta(seconds=5)
-        a = send_thanks_to_customer_whatsapp.apply_async(args=[msisdn], eta=send_date)
-        aresult = a.get()
-
-        b = add.apply_async((1, 3), eta=send_date)
-        bresult = b.get()
-        """
+        async_result = send_thanks_to_customer_whatsapp.apply_async(args=[msisdn], eta=send_date)
+        async_result.get()
 
         return super().save(*args, **kwargs)
 
