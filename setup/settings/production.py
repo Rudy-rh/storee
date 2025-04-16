bc5600fc1c118a9183224593c52b1480c1a619a9
Poralia <poraliaglobal@gmail.com>
2025-04-16 19:24:10 +0700

    test cors

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -65,11 +65,12 @@ CSRF_TRUSTED_ORIGINS = [
 # Django CORS
 # ------------------------------------------------------------------------------
 # https://pypi.org/project/django-cors-headers/
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost',
     'https://localhost',
+    'https://localhost/',
     'http://localhost:8100',
     'https://localhost:8100',
     'https://storee-46b6e.web.app',
