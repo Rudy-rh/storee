2553a162abc72bb3bc5a6423a047fc0cf1294a9f
Rudy-rh <69201565+Rudy-rh@users.noreply.github.com>
2026-08-28 15:35:49 +0900

    Update send_verifycode_whatsapp function for Fonnte API

--- apps/person/fix_fonnte.py
+++ apps/person/fix_fonnte.py
@@ -1,63 +1,9 @@
+old_func = '@shared_task\ndef send_verifycode_whatsapp(data):\n    logging.info(_("Send verifyCode whatsapp run"))\n\n    to = data.get(\'msisdn\', None)\n    passcode = data.get(\'passcode\', None)\n\n    # modify msisdn\n    if to.startswith(\'0\'):\n        to = to.replace(\'0\', \'62\', 1)\n\n    if to and passcode:\n        url = \'https://api.zuwinda.com/v2/messaging/whatsapp/message\'\n\n        payload = {\n            "content": "Kode Verifikasi Storee Barber %s Jangan berikan kepada siapapun!" % passcode,\n            "accountId": "509849e3-040a-4793-a9d5-ddace5c5bf98",\n        "messageType": "text",\n            "to": to\n        }\n        headers = {\n            \'Accept\': \'application/json\',\n            \'Content-Type\': \'application/json\',\n            \'x-access-key\': settings.ZUWINDA_KEY\n        }\n        r = requests.post(url, json=payload, headers=headers)\n        logging.info(r.status_code)\n    else:\n        logging.warning(\n            _("Tried to send whatsapp to non-existing VerifyCode Code"))\n\n'
+
+new_func = '@shared_task\ndef send_verifycode_whatsapp(data):\n    logging.info(_("Send verifyCode whatsapp run"))\n\n    to = data.get(\'msisdn\', None)\n    passcode = data.get(\'passcode\', None)\n\n    # modify msisdn\n    if to.startswith(\'0\'):\n        to = to.replace(\'0\', \'62\', 1)\n\n    if to and passcode:\n        url = \'https://api.fonnte.com/send\'\n\n        payload = {\n            "target": to,\n            "message": "Kode Verifikasi Storee Barber %s Jangan berikan kepada siapapun!" % passcode\n        }\n        headers = {\n            \'Authorization\': settings.FONNTE_TOKEN\n        }\n        r = requests.post(url, data=payload, headers=headers)\n        logging.info(r.status_code)\n    else:\n        logging.warning(\n            _("Tried to send whatsapp to non-existing VerifyCode Code"))\n\n'
+
 with open('apps/person/tasks.py', 'r') as f:
     content = f.read()
-
-old_func = '''@shared_task
-def send_verifycode_whatsapp(data):
-    logging.info(_("Send verifyCode whatsapp run"))
-
-    to = data.get('msisdn', None)
-    passcode = data.get('passcode', None)
-
-    # modify msisdn
-    if to.startswith('0'):
-        to = to.replace('0', '62', 1)
-
-    if to and passcode:
-        url = 'https://api.zuwinda.com/v2/messaging/whatsapp/message'
-
-        payload = {
-            "content": "Kode Verifikasi Storee Barber %s Jangan berikan kepada siapapun!" % passcode,
-            "accountId": "509849e3-040a-4793-a9d5-ddace5c5bf98",
-        "messageType": "text",
-            "to": to
-        }
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'x-access-key': settings.ZUWINDA_KEY
-        }
-        r = requests.post(url, json=payload, headers=headers)
-        logging.info(r.status_code)
-    else:
-        logging.warning(
-            _("Tried to send whatsapp to non-existing VerifyCode Code"))'''
-
-new_func = '''@shared_task
-def send_verifycode_whatsapp(data):
-    logging.info(_("Send verifyCode whatsapp run"))
-
-    to = data.get('msisdn', None)
-    passcode = data.get('passcode', None)
-
-    # modify msisdn
-    if to.startswith('0'):
-        to = to.replace('0', '62', 1)
-
-    if to and passcode:
-        url = 'https://api.fonnte.com/send'
-
-        payload = {
-            "target": to,
-            "message": "Kode Verifikasi Storee Barber %s Jangan berikan kepada siapapun!" % passcode
-        }
-        headers = {
-            'Authorization': settings.FONNTE_TOKEN
-        }
-        r = requests.post(url, data=payload, headers=headers)
-        logging.info(r.status_code)
-    else:
-        logging.warning(
-            _("Tried to send whatsapp to non-existing VerifyCode Code"))'''
 
 if old_func in content:
     content = content.replace(old_func, new_func)
@@ -65,10 +11,4 @@ if old_func in content:
         f.write(content)
     print("BERHASIL")
 else:
-    print("GAGAL - tidak cocok persis")
-    # Debug: cari fungsi di file untuk lihat isi sebenarnya
-    import re
-    match = re.search(r'@shared_task\ndef send_verifycode_whatsapp.*?(?=\n@shared_task|\Z)', content, re.DOTALL)
-    if match:
-        print("--- ISI FUNGSI SEBENARNYA ---")
-        print(repr(match.group()))
+    print("MASIH GAGAL - butuh cara lain")
